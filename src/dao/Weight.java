b2533be3c9571e68ef73065c262a233b54ee2289
Mortenbaws <morten2094@hotmail.com>
2016-06-06 12:35:22 +0200

    Added Weight Class and DAO for the weight

--- src/dao/Weight.java
+++ src/dao/Weight.java
@@ -2,4 +2,31 @@ package dao;
 
 public class Weight {
 
+	private double tara; //emballage af varen
+	private double netto; //indholdet af varen
+	
+	public Weight(){
+		tara = 0;
+		netto = 0;
+	}
+	
+	public double getNetto(){
+		return netto;
+	}
+	
+	public double getTara(){
+		return tara;
+	}
+	
+	public void setNetto(double netto){
+		this.netto = netto;
+	}
+	
+	public void setTara(double tara){
+		this.tara = tara;
+	}
+	
+	public double getBrutto(){
+		return tara + netto;
+	}
 }
--- src/dto/WeightHandler.java
+++ src/dto/WeightHandler.java
@@ -1,6 +1,26 @@
 package dto;
 
+import dao.Weight;
 
 public class WeightHandler {
 
+	private Weight weight;
+	
+	public WeightHandler(){
+		this.weight = new Weight();
+	}
+	
+	public void add(double weight){
+		this.weight.setNetto(this.weight.getNetto()+weight);
+	}
+	
+	public void tarer(){
+		weight.setTara(weight.getNetto());
+		weight.setNetto(0);
+	}
+	
+	public void reset(){
+		weight.setTara(0);
+		weight.setNetto(0);
+	}
 }
