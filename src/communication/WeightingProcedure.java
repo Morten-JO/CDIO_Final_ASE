8119bd2afe2ed9b08b388fafca28e9e36972271e
Mortenbaws <morten2094@hotmail.com>
2016-06-15 13:08:49 +0200

    Fixed error!

--- src/communication/WeightingProcedure.java
+++ src/communication/WeightingProcedure.java
@@ -44,7 +44,6 @@ public class WeightingProcedure implements Runnable {
 					handleMeasuringLoop();
 				}
 			} catch (IOException | NullPointerException e) {
-				e.printStackTrace();
 				running = false;
 				validOprId = true;
 				handler.setProduktBatchStatus(transactionProduktBatchID, 0);
